e6da4d37c7294310f9fc49412d3a1433a7d681e0
Fabio <fabio.deangelis.elettronike@gmail.com>
2023-09-30 02:17:22 +0200

    file update

---
+++ jim-config.h
@@ -0,0 +1,13 @@
+#ifndef _JIM_CONFIG_H
+#define _JIM_CONFIG_H
+
+//#define HAVE_LONG_LONG
+#define JIM_UTF8 1
+#define JIM_VERSION 82
+#define SIZEOF_INT 4
+
+#define USE_LINENOISE
+
+#define JIM_MATH_FUNCTIONS
+
+#endif
